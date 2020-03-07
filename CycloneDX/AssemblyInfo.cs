3980d998ebb609f0e4e410bde0f8296c36d670f2
Patrick Dwyer <coderpatros@outlook.com>
2020-03-07 23:15:19 +1000

    Suppress literal string globalization warnings

--- CycloneDX/AssemblyInfo.cs
+++ CycloneDX/AssemblyInfo.cs
@@ -1,2 +1,3 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("CycloneDX.Tests")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("CycloneDX.IntegrationTests")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:DoNotPassLiteralsAsLocalizedParameters")]
