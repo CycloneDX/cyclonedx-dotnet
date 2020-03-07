3fde5ba638e1d4680f0229ff17ecaa4cdfc6e8e6
Patrick Dwyer <coderpatros@outlook.com>
2020-03-07 23:35:58 +1000

    Suppress dispose objects warning
    In the context of the CLI tool this is desired behaviour. The HttpClient
    shouldn't be disposed of early. But this needs to be revisited in the
    context of a library with correct scoping of the HttpClient instance.

--- CycloneDX.Core/AssemblyInfo.cs
+++ CycloneDX.Core/AssemblyInfo.cs
@@ -1,2 +1,4 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("CycloneDX.Core.Tests")]
+// TODO: in the context of a CLI tool this wasn't as much of a problem, we wanted the http client to hang around and be re-used, but needs to be revisited for the core library
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:DisposeObjectsBeforeLosingScope")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:DoNotPassLiteralsAsLocalizedParameters")]
--- CycloneDX/AssemblyInfo.cs
+++ CycloneDX/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("CycloneDX.Tests")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("CycloneDX.IntegrationTests")]
+// TODO: in the context of a CLI tool this wasn't as much of a problem, we wanted the http client to hang around and be re-used, but needs to be revisited for the core library
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:DisposeObjectsBeforeLosingScope")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:DoNotPassLiteralsAsLocalizedParameters")]
