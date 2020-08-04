0c1939293bf5636126a447bee16eddda024105c5
Patrick Dwyer <coderpatros@outlook.com>
2020-08-04 21:02:38 +1000

    Use workflow dispatch event for releases

--- trigger-release-workflow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# requires the environment variable GITHUB_TOKEN to be set
-# (the personal access token requires repo scope)
-curl -H "Accept: application/vnd.github.everest-preview+json" \
-    -H "Authorization: token $GITHUB_TOKEN" \
-    --request POST \
-    --data '{"event_type": "release"}' \
-    https://api.github.com/repos/CycloneDX/cyclonedx-dotnet/dispatches
