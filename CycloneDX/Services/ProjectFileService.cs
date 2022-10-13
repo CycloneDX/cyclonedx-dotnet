9972cdf8a59ba5214f62d8b3c2e3dcb729c39685
Aristotelos <arisvd@gmail.com>
2022-10-13 15:48:01 +0200

    Do not write an error if a project has no project references.
    Signed-off-by: Aristotelos <arisvd@gmail.com>

--- CycloneDX/Services/ProjectFileService.cs
+++ CycloneDX/Services/ProjectFileService.cs
@@ -212,7 +212,7 @@ namespace CycloneDX.Services
 
             if (projectReferences.Count == 0)
             {
-                Console.Error.WriteLine("  No project references found");
+                Console.WriteLine("  No project references found");
             }
 
             return projectReferences;
