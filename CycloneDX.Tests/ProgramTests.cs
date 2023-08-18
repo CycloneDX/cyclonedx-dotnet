75651443099791b169b8a20cec3c6fcfc23a838d
Bert <bert.k@hotmail.com>
2023-08-18 07:02:51 +0200

    eliminate removed package #750
    Signed-off-by: Bert <bert.k@hotmail.com>

--- CycloneDX.Tests/ProgramTests.cs
+++ CycloneDX.Tests/ProgramTests.cs
@@ -15,16 +15,16 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (c) OWASP Foundation. All Rights Reserved.
 
+using System;
 using System.Collections.Generic;
-using Xunit;
+using System.IO;
 using System.IO.Abstractions.TestingHelpers;
 using System.Threading.Tasks;
-using XFS = System.IO.Abstractions.TestingHelpers.MockUnixSupport;
-using Moq;
-using CycloneDX.Models;
-using System.IO;
 using CycloneDX.Interfaces;
-using Microsoft.DotNet.PlatformAbstractions;
+using CycloneDX.Models;
+using Moq;
+using Xunit;
+using XFS = System.IO.Abstractions.TestingHelpers.MockUnixSupport;
 
 namespace CycloneDX.Tests
 {
@@ -33,7 +33,7 @@ namespace CycloneDX.Tests
         [Fact]
         public async Task CallingCycloneDX_WithoutSolutionFile_ReturnsSolutionOrProjectFileParameterMissingExitCode()
         {
-            var exitCode = await Program.Main(new string[] {}).ConfigureAwait(false);
+            var exitCode = await Program.Main(new string[] { }).ConfigureAwait(false);
 
             Assert.Equal((int)ExitCode.SolutionOrProjectFileParameterMissing, exitCode);
         }
@@ -101,7 +101,7 @@ namespace CycloneDX.Tests
         public void CheckMetaDataTemplate()
         {
             var bom = new Bom();
-            string resourcePath = Path.Join(ApplicationEnvironment.ApplicationBasePath, "Resources", "metadata");
+            string resourcePath = Path.Join(AppContext.BaseDirectory, "Resources", "metadata");
             bom = Program.ReadMetaDataFromFile(bom, Path.Join(resourcePath, "cycloneDX-metadata-template.xml"));
             Assert.NotNull(bom.Metadata);
             Assert.Matches("CycloneDX", bom.Metadata.Component.Name);
